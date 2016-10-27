00a15be38ce051b23dd5bd9e3f1f910cc423bc5c
Luke Dalessandro <ldalessa@indiana.edu>
2016-10-27 15:02:01 -0500

    Add a patch for googletest to avoid collisions with #include <complex.h>.

--- cmake/Modules/GTest.cmake
+++ cmake/Modules/GTest.cmake
@@ -2,7 +2,8 @@ function(gtest)
   download_project(
     PROJ googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG release-1.8.0
+    GIT_TAG master
+    PATCH_COMMAND "git apply ${CMAKE_SOURCE_DIR}/cmake/Patches/googletest-complex.patch"
     ${UPDATE_DISCONNECTED_IF_AVAILABLE})
 
   # Prevent GoogleTest from overriding our compiler/linker options
