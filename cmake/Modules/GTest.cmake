e8d0d7508381607d1a0c03aeffe394e13a4808ec
Luke D'Alessandro <ldalessa@uw.edu>
2018-08-14 11:25:47 -0700

    Reworking of the CMake infrastructure.
    This updates the minimum required cmake to 3.8, and uses some more standardized
    functionality to, e.g., check for C++14.

--- cmake/Modules/GTest.cmake
+++ cmake/Modules/GTest.cmake
@@ -1,24 +1,13 @@
-function(gtest)
-  download_project(
-    PROJ googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG master
-    PATCH_COMMAND "git apply ${CMAKE_SOURCE_DIR}/cmake/Patches/googletest-complex.patch"
-    ${UPDATE_DISCONNECTED_IF_AVAILABLE})
+include(DownloadProject)
+download_project(
+  PROJ googletest
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG master
+  PATCH_COMMAND "git apply ${CMAKE_SOURCE_DIR}/cmake/Patches/googletest-complex.patch"
+  UPDATE_DISCONNECTED 1)
 
-  # Prevent GoogleTest from overriding our compiler/linker options
-  # when building with Visual Studio
-  set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+# Prevent GoogleTest from overriding our compiler/linker options
+# when building with Visual Studio
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
-  add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
-
-  # When using CMake 2.8.11 or later, header path dependencies
-  # are automatically added to the gtest and gmock targets.
-  # For earlier CMake versions, we have to explicitly add the
-  # required directories to the header search path ourselves.
-  if (CMAKE_VERSION VERSION_LESS 2.8.11)
-    include_directories("${gtest_SOURCE_DIR}/include"
-      "${gmock_SOURCE_DIR}/include")
-  endif()
-
-endfunction()
+add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
