3d0c41de6a29caaefa8278afca789523fdda237f
Luke Dalessandro <ldalessa@iu.edu>
2018-09-13 14:50:36 -0400

    First working CUDA test.

--- test/Init.cu
+++ test/Init.cu
@@ -2,36 +2,27 @@
 #include <gtest/gtest.h>
 
 __global__
-void test_init() {
-  ttl::Tensor<2,2,double> A;
-  ttl::Tensor<2,2,double> B = { 1.0, 2.0, 3.0, 4.0 };
-  A=B;
-}
-
-
-__global__
-void test_2(double *a, double *b) {
-  ttl::Tensor<2,2,double*> A = { a };
-  ttl::Tensor<2,2,double*> B = { b, { 1.0, 2.0, 3.0, 4.0 }};
-  A=B;
+void test_init(double *a) {
+  ttl::Tensor<2,2,double*> A = { a, { 1.0, 2.0, 3.0, 4.0 }};
 }
 
 TEST(Tensor, Init) {
-  test_init<<<1, 1>>>();
+  int nDevices;
+  cudaGetDeviceCount(&nDevices);
+  assert(nDevices > 0);
 
-  double *a, *b;
-  cudaMalloc(&a, 4*sizeof(double));
-  cudaMalloc(&b, 4*sizeof(double));
-  test_2<<<1, 1>>>(a, b);
+  double *a;                                    // allocate device space
+  cudaMallocManaged(&a, 4*sizeof(double));
+  assert(a);
+  a[0] = 0.0;
+  a[1] = 0.0;
+  a[2] = 0.0;
+  a[3] = 0.0;
+  test_init<<<1, 1>>>(a);
+  cudaDeviceSynchronize();
   EXPECT_EQ(a[0], 1.0);
   EXPECT_EQ(a[1], 2.0);
   EXPECT_EQ(a[2], 3.0);
   EXPECT_EQ(a[3], 4.0);
-
-  EXPECT_EQ(b[0], 1.0);
-  EXPECT_EQ(b[1], 2.0);
-  EXPECT_EQ(b[2], 3.0);
-  EXPECT_EQ(b[3], 4.0);
   cudaFree(a);
-  cudaFree(b);
 }
