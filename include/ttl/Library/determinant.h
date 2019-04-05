fd843cc6298e1a249e94b0432ff83ce682f7c8b5
Luke D'Alessandro <ldalessa@uw.edu>
2019-04-04 20:00:07 -0700

    Update the determinant to be equivalent to that used in other cswarm code.

--- include/ttl/Library/determinant.h
+++ include/ttl/Library/determinant.h
@@ -58,13 +58,10 @@ template <class E>
 struct det_impl<E, 2, 3>
 {
   static auto op(E f) {
-    auto t0 = f(0,0)*f(1,1)*f(2,2);
-    auto t1 = f(1,0)*f(2,1)*f(0,2);
-    auto t2 = f(2,0)*f(0,1)*f(1,2);
-    auto s0 = f(0,0)*f(1,2)*f(2,1);
-    auto s1 = f(1,1)*f(2,0)*f(0,2);
-    auto s2 = f(2,2)*f(0,1)*f(1,0);
-    return (t0 + t1 + t2) - (s0 + s1 + s2);
+    auto t0 = f(0,0)*(f(1,1)*f(2,2) - f(1,2)*f(2,1));
+    auto t1 = f(1,0)*(f(2,1)*f(0,2) - f(2,2)*f(0,1));
+    auto t2 = f(2,0)*(f(0,1)*f(1,2) - f(1,1)*f(0,2));
+    return t0 + t1 + t2;
   }
 };
 } // namespace lib
